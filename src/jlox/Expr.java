8f66a73074d31ee307423101b4133b3a48fdb406
Lorenzo <forsomereasontwas@gmail.com>
2026-08-05 08:55:52 +0500

    Added AstPrinter test

--- src/jlox/Expr.java
+++ src/jlox/Expr.java
@@ -1,11 +1,36 @@
 package jlox;
 
 abstract class Expr {
+
   interface Visitor<R> {
       R visitBinaryExpr(Binary expr);
       R visitGroupingExpr(Grouping expr);
       R visitLiteralExpr(Literal expr);
       R visitUnaryExpr(Unary expr);
+  }
+
+
+  class ReversePolish implements Visitor {
+
+    @Override
+    public String visitBinaryExpr(Binary expr) {
+      return expr.left.toString() + expr.right.toString() + expr.operator.toString();
+    }
+
+    @Override
+    public String visitGroupingExpr(Grouping expr) {
+     return expr.toString();
+    }
+
+    @Override
+    public String visitLiteralExpr(Literal expr) {
+     return expr.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Unary expr) {
+      return expr.right.toString() + expr.operator.toString();
+    }
   }
 
  static class Binary extends Expr {

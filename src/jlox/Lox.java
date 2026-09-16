0af7c84eda03c9800c5e1d6cfffb7bcea4284d49
Lorenzo <forsomereasontwas@gmail.com>
2026-09-16 06:03:24 +0500

    integrated interpreter into pipeline

--- src/jlox/Lox.java
+++ src/jlox/Lox.java
@@ -8,7 +8,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-    static boolean hadError = false;
+  private static final Interpreter interpreter = new Interpreter();
+  static boolean hadError = false;
+  static boolean hadRuntimeError = false;
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -20,14 +23,15 @@ public class Lox {
     }
   }
 
-private static void runFile(String path) throws IOException {
-    if (hadError) System.exit(65);
+  private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
-    // explore line by line reading
+
+    if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
 
- private static void runPrompt() throws IOException {
+  private static void runPrompt() throws IOException {
     // compare to newer methods (scanner)
     InputStreamReader input = new InputStreamReader(System.in);
     BufferedReader reader = new BufferedReader(input);
@@ -52,12 +56,17 @@ private static void runFile(String path) throws IOException {
     // Stop if there was a syntax error.
     if (hadError) return;
 
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
   }
- 
+
 
   public static void error(int line, String message) {
     report(line, "", message);
+  }
+
+  public static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() + "\n[line" + error.token.line + "]");
+    hadRuntimeError = true;
   }
 
   private static void report(int line, String where, String message) {

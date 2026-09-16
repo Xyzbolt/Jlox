03e42e225b06904df37d917d18a98723fa89aa19
Lorenzo <forsomereasontwas@gmail.com>
2026-09-16 06:03:07 +0500

    added interpret method

--- src/jlox/Interpreter.java
+++ src/jlox/Interpreter.java
@@ -2,6 +2,29 @@ package jlox;
 
 class Interpreter implements Expr.Visitor<Object> {
 
+  public void  interpret(Expr expression) {
+    try {
+    Object value = evaluate(expression);
+    System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
+
+  private String stringify(Object object) {
+    if (object == null) return "nil";
+
+    if (object instanceof Double) {
+      String text = object.toString();
+      if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+
+    return object.toString();
+  }
+
   private Object evaluate(Expr expr) {
     return expr.accept(this);
   }
@@ -24,6 +47,11 @@ class Interpreter implements Expr.Visitor<Object> {
     throw new RuntimeError(operator, "operator must be a number");
   }
 
+  private void checkNumberOperand(Token operator, Object left, Object right) {
+    if (left instanceof Double && right instanceof Double) return;
+    throw new RuntimeError(operator, "operator must be a number");
+  }
+
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
@@ -35,10 +63,10 @@ class Interpreter implements Expr.Visitor<Object> {
 
     switch (expr.operator.type) {
       case BANG:
-        return !isTruthy(right);
+      return !isTruthy(right);
       case MINUS:
-        checkNumberOperand(expr.operator, right);
-        return -(double)right;
+      checkNumberOperand(expr.operator, right);
+      return -(double)right;
     }
 
     return null;
@@ -51,27 +79,34 @@ class Interpreter implements Expr.Visitor<Object> {
 
     switch (expr.operator.type) {
       case PLUS:
-        if (left instanceof Double && right instanceof Double) {
-          return (double)left + (double)right;
-        }
+      if (left instanceof Double && right instanceof Double) {
+        return (double)left + (double)right;
+      }
 
-        if (left instanceof String && right instanceof String) {
-          return (String)left + (String)right;
-        }
-      break;
+      if (left instanceof String && right instanceof String) {
+        return (String)left + (String)right;
+      }
+      throw new RuntimeError(expr.operator, "Operands both need to be either strings of numbers.");
       case MINUS:
+      checkNumberOperand(expr.operator, left, right);
       return (double)left - (double)right;
       case SLASH:
+      checkNumberOperand(expr.operator, left, right);
       return (double)left / (double)right;
       case STAR:
+      checkNumberOperand(expr.operator, left, right);
       return (double)left * (double)right;
       case GREATER:
+      checkNumberOperand(expr.operator, left, right);
       return (double)left > (double)right;
       case GREATER_EQUAL:
+      checkNumberOperand(expr.operator, left, right);
       return (double)left >= (double)right;
       case LESS:
+      checkNumberOperand(expr.operator, left, right);
       return (double)left < (double)right;
       case LESS_EQUAL:
+      checkNumberOperand(expr.operator, left, right);
       return (double)left <= (double)right;
       case EQUAL_EQUAL:
       return isEqual(left, right);
